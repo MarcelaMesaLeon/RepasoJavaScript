06dba44a73fa8ea14379ee8b1c688b085a852b6f
MarcelaMesaLeon <mardie05@hotmail.com>
2022-01-11 19:14:29 -0500

    Se crean archivos para repaso de JavaScript

---
+++ primer.js
@@ -0,0 +1,35 @@
+let year= prompt("Hola, en qué año naciste?");
+        console.log("año:"+ year);
+        let nacimiento= year;
+        let actual= 2022;
+        let edad=(actual-nacimiento);
+        document.write("Tu edad es:"+ edad);
+
+let nombre= prompt("Cuál es tu nombre?");
+let usuario= {
+    "nombre": nombre, 
+    "edad": edad,
+    "esEmpleado": true
+}
+let usuarios =[];
+console.log(usuario);
+usuarios.push(usuario);
+console.log(usuarios);
+
+year= prompt("Hola, en qué año naciste?");
+        console.log("año:"+ year);
+        nacimiento= year;
+        edad=(actual-nacimiento);
+        document.write("Tu edad es:"+ edad);
+
+nombre= prompt("Cuál es tu nombre?");
+usuario= {
+    "nombre": nombre, 
+    "edad": edad,
+    "esEmpleado": true
+
+}
+
+usuarios.push(usuario);
+console.log(usuarios);
+document.write("Tu nombre es:"+ usuario.nombre);
