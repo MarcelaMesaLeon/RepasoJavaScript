72e561a6b37988791b3f4c146a97a5073eb7655b
MarcelaMesaLeon <mardie05@hotmail.com>
2022-01-12 19:28:03 -0500

    Se agregan archivos clase2

---
+++ Clases/js2.js
@@ -0,0 +1,16 @@
+let nombre = prompt("Cual es tu nombre?");
+let fecha= prompt("En que año naciste?");
+    let actual= 2022;
+    let edad= (actual-fecha); 
+    document.write(nombre+ ", Tu edad es: "+ edad);
+
+if(edad > 18){
+    document.write("Eres mayor de edad");
+} else {
+    document.write("No eres mayor de edad");
+}
+
+
+
+
+
